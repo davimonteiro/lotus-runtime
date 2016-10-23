6fdf0ae3389fae7dad5ba4e9f541246957f5f7ef
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2016-10-23 18:47:51 -0300

    Code refactoring

--- src/main/java/br/com/davimonteiro/lotus_runtime/notifier/ViolationHandler.java
+++ src/main/java/br/com/davimonteiro/lotus_runtime/notifier/ViolationHandler.java
@@ -1,6 +1,6 @@
 /**
  * The MIT License
- * Copyright (c) 2015 Davi Monteiro Barbosa
+ * Copyright © 2016 Davi Monteiro Barbosa
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
