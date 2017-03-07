5c77470fc3170c9267c411a3e196caa289c8bc96
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2017-03-07 07:39:01 -0300

    Update the README and the license.

--- src/main/java/br/com/davimonteiro/lotus_runtime/config/ConfigurationServiceComponentImpl.java
+++ src/main/java/br/com/davimonteiro/lotus_runtime/config/ConfigurationServiceComponentImpl.java
@@ -1,6 +1,6 @@
 /**
  * The MIT License
- * Copyright © 2016 Davi Monteiro Barbosa
+ * Copyright © 2017 Davi Monteiro Barbosa
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
